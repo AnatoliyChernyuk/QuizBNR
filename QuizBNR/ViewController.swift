0ac14ace3d93e3713a6a184c45ce7831d6612b38
AnatoliyChernyuk <toliatche@gmail.com>
2017-12-22 23:28:45 +0200

    Project start

--- QuizBNR/ViewController.swift
+++ QuizBNR/ViewController.swift
@@ -9,17 +9,83 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak  var questionLabel: UILabel!
+    
+    var currentQuestionIndex: Int = 0
+    
+    let questions = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is congniac made from?"
+    ]
+    
+    let answers = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        answerLabel.text = answers[currentQuestionIndex]
     }
-
-
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        let question = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
